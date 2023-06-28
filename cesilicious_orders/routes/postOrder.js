29defe66bf15ca5c7a3b1582960818f1fc67d813
Tristan ROULET <tristan.roulet@viacesi.fr>
2023-06-28 09:50:29 +0200

    add auto creation of Id order

--- cesilicious_orders/routes/postOrder.js
+++ cesilicious_orders/routes/postOrder.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const Order = require('../schema/order');
+const { ObjectId } = require('mongodb');
 
 // Create an order
 router.post('/', async (req, res) => {
   try {
-    const order = new Order(req.body);
+    let orderJSON = req.body;
+    orderJSON._id = new ObjectId().toString();
+    const order = new Order(orderJSON);
     const savedOrder = await order.save();
     res.status(201).json(savedOrder);
   } catch (error) {
