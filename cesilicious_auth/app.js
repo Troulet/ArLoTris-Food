618a6c39493665779fe98941e339b38ab73db757
Tristan ROULET <tristan.roulet@viacesi.fr>
2023-06-28 09:07:55 +0200

    add Cors auth

--- cesilicious_auth/app.js
+++ cesilicious_auth/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
 
 const authUserRouter = require('./routes/authUser');
 const postUserRouter = require('./routes/postUser');
@@ -13,6 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 app.use('/login', authUserRouter);
 app.use('/user', postUserRouter);
